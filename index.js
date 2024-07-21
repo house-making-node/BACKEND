cad527de976016cd0fe662ffdcd2bff8bf39d48f
lilloo04 <lilloo04@naver.com>
2024-07-21 21:07:05 +0900

    Initial Setting

---
+++ index.js
@@ -0,0 +1,18 @@
+// const express = require('express')   // common JS
+import express from 'express'          // ES6
+import {tempRouter} from './src/routes/temp.route.js'
+
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+// router setting
+app.use('/temp', tempRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
