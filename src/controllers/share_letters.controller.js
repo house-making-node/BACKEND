860377e21a8fff77078a5a88f634a29fc3d773ed
llayeon <layon0318@naver.com>
2024-07-25 22:41:47 +0900

    [FEAT] 공유레터 구독정보 저장하기 API 구현완료

---
+++ src/controllers/share_letters.controller.js
@@ -0,0 +1,17 @@
+import { response } from '../../config/response.js';
+import { status } from '../../config/response.status.js';
+import { addInfo } from '../services/share_subscriptions.service.js';
+import { addLetter } from '../services/share_letters.service.js';
+
+export const addSubscriptionInfo = async (req,res,next) => {
+    try{
+        console.log("사용자의 구독정보가 저장됩니다.");
+        console.log("body: ",req.body);
+
+        const info = await addInfo(req.body);
+        res.send(response(status.SUCCESS,info));
+    }catch (error){
+        next(error);
+    }
+};
+
--- src/controllers/temp.controller.js
+++
@@ -1,9 +0,0 @@
-// temp.controller.js
-
-import { status } from '../../config/response.status.js';
-import { getTempData } from '../services/temp.service.js';
-import { response } from '../../config/response.js';
-
-export const tempTest = (req, res, next) => {
-    res.send(response(status.SUCCESS, getTempData()));
-};
