9ae8c79743b3552ad21886e98d814c6ad1a308c3
lilloo04 <lilloo04@naver.com>
2024-07-26 11:30:28 +0900

    feat: 1차 faq게시판 API

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // const express = require('express')   // common JS
-import express from 'express'          // ES6
-import {tempRouter} from './src/routes/temp.route.js'
+import express from "express"; // ES6
+import { tempRouter } from "./src/routes/temp.route.js";
+import { faqRouter } from "./src/routes/faq.route.js";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 // router setting
-app.use('/temp', tempRouter);
+app.use("/temp", tempRouter);
+app.use("faq", faqRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
