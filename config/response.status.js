f84572d313d02b927186a0fdd175341b54acd2f7
JEEWOOCHOI <jeewoochoi47@gmail.com>
2024-07-26 10:48:26 +0900

    [FEAT] 컨설팅 요구사항 (집 평수) 저장 API 구현

--- config/response.status.js
+++ config/response.status.js
@@ -5,5 +5,7 @@ import { StatusCodes } from "http-status-codes";
 export const status = {
     // success
     SUCCESS: {status: StatusCodes.OK, "isSuccess": true, "code": 2000, "message": "success!"},    
+    BAD_REQUEST: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "COMMON001", "message": "잘못된 요청입니다." },
+    USER_NOT_FOUND: {status: StatusCodes.BAD_REQUEST, "isSuccess": false, "code": "MEMBER4001", "message": "사용자가 없습니다."},
 
 }
--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-// const express = require('express')   // common JS
 import express from 'express'          // ES6
-import {tempRouter} from './src/routes/temp.route.js'
+import dotenv from 'dotenv';
+import { specs } from "./config/swagger.config.js";
+import SwaggerUi from "swagger-ui-express";
+import { BaseError } from './config/error.js';
+import { status } from './config/response.status.js';
+import cors from 'cors';
+import { consultRouter } from './src/routes/consult.route.js';
+
+dotenv.config();
 
 const app = express()
-const port = 3000
+app.set('port', process.env.PORT || 3000)   // 서버 포트 지정
+app.use(cors());
+app.use(express.static('public'));          // 정적 파일 접근
+app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
-// router setting
-app.use('/temp', tempRouter);
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(specs));
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+//컨설팅
+app.use('/consulting',consultRouter);
 
+// error handling
+app.use((req, res, next) => {
+    const err = new BaseError(status.NOT_FOUND);
+    next(err);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Example app listening on port ${app.get('port')}`);
+});
