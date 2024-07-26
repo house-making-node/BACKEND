40b56a0b2fed67c9b14355773966e35c03da5787
JEEWOOCHOI <jeewoochoi47@gmail.com>
2024-07-26 15:25:48 +0900

    [FEAT] 컨설팅 요구사항 (방 개수) 저장 API 구현

--- src/models/consult.sql.js
+++ src/models/consult.sql.js
@@ -1,3 +1,4 @@
 export const getUserWithIdQ="SELECT * FROM USER WHERE user_id=?";
 export const setHouseSizeQ="INSERT INTO CONSULTING(user_id,house_size,status) VALUES(?,?,?)";
 export const getConsultReqWithIdQ="SELECT * FROM CONSULTING WHERE id=?";
+export const setRoomNumberQ="UPDATE CONSULTING SET room_num = ?, status = ? WHERE id = ?";
