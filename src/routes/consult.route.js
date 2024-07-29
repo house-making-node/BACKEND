eec52b1100ac04331e751dc8cab9de7b7675107c
JEEWOOCHOI <jeewoochoi47@gmail.com>
2024-07-29 15:23:01 +0900

    [FEAT] 컨설팅 요구사항 (분위기) 저장 API 구현

--- src/routes/consult.route.js
+++ src/routes/consult.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import { houseSizeAdd, roomNumberAdd } from '../controllers/consult.controller.js';
+import { houseSizeAdd, moodAdd, roomNumberAdd } from '../controllers/consult.controller.js';
 
 export const consultRouter = express.Router();
 consultRouter.post('/requirements/house_size', expressAsyncHandler(houseSizeAdd));
 consultRouter.patch('/requirements/room_num', expressAsyncHandler(roomNumberAdd));
+consultRouter.patch('/requirements/mood', expressAsyncHandler(moodAdd));
--- src/services/consult.service.js
+++ src/services/consult.service.js
@@ -1,7 +1,7 @@
 import { BaseError } from "../../config/error.js";
 import { status } from "../../config/response.status.js";
 import { addConsultReqResponseDTO } from "../dtos/consult.response.dto.js";
-import { getConsultReq, getUser, setHouseSize, setRoomNumber } from "../models/consult.dao.js";
+import { getConsultReq, getUser, setHouseSize, setMood, setRoomNumber } from "../models/consult.dao.js";
 
 export const addHouseSize=async (body)=>{
     //유저 정보가 있는지 확인
@@ -39,4 +39,18 @@ export const addRoomNumber=async (body)=>{
     });
     //추가후 반환된 consulting_id를 이용하여 컨설팅 정보를 가져옴
     return addConsultReqResponseDTO(await getConsultReq(addRoomData));
+}
+
+export const addMood=async (body)=>{
+    const getConsultData=await getConsultReq(body.consulting_id);
+    if(getConsultData==-1){
+        throw new BaseError(status.CONSULT_NOT_FOUND);
+    }
+    const addMoodData=await setMood({
+        'consulting_id':body.consulting_id,
+        'mood':body.mood,
+        'status':'step2',
+    });
+    console.log(addMoodData);
+    return addConsultReqResponseDTO(await getConsultReq(addMoodData));
 }
