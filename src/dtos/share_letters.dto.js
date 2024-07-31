109c584cd184e88d6bba29f84eb271c75d021aba
llayeon <layon0318@naver.com>
2024-08-01 00:15:01 +0900

    [FEAT] 공유레터 리스트 가져오기 api 구현

--- src/dtos/share_letters.dto.js
+++ src/dtos/share_letters.dto.js
@@ -10,3 +10,24 @@ export const addLetterDataResponseDTO = (letterInfo) => {
         's3_key' : letterInfo[0].s3_key
     };
 }
+
+export const previewLettersResponseDTO = (data) => {
+    const letters = [];
+
+    for(let i = 0; i < data.length; i++) {
+        letters.push({
+            "letter_id": data[i].letter_id,
+            "share_id": data[i].share_id,
+            "title" : data[i].title,
+            "s3_key" : data[i].s3_key,
+            "content" : data[i].content,
+            "create_data" : formatDate(data[i].created_at)
+        })
+    }
+
+    return {"Letter": letters}
+}
+
+const formatDate = (date) => {
+    return new Intl.DateTimeFormat('kr').format(new Date(date)).replaceAll(" ", "").slice(0, -1);
+}
--- src/models/share_letters.sql.js
+++ src/models/share_letters.sql.js
@@ -14,3 +14,10 @@ export const insertSharedLetterContentSql = `
     INSERT INTO SHARED_LETTER (share_id, content, s3_key, title)
     VALUES (?, ?, ?, ?)
 `;
+
+export const selectLettersPreviewSql = `
+    SELECT letter_id, share_id, title, s3_key, content, created_at
+    FROM SHARED_LETTER
+    ORDER BY created_at DESC
+    LIMIT ? OFFSET ?
+`;
