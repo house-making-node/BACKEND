96dc20aa960576b2e20380ef59c23650e4151ca9
Baetato <diana021015@gmail.com>
2024-08-08 02:19:12 +0900

    [FEAT] 전체 자취레터 리스트 조회하기 api 구현

--- src/services/home_letters.service.js
+++ src/services/home_letters.service.js
@@ -3,6 +3,7 @@
 import { getLetterById } from '../models/home_letters.dao.js';
 import { BaseError } from "../../config/error.js";
 import { status } from "../../config/response.status.js";
+import { getHomeLetters } from '../models/home_letters.dao.js';
 
 export const getLetterDetail = async (letter_id) => {
     console.log("getLetterDetail [letter_id]: ", letter_id); //에러로그확인용
@@ -13,4 +14,8 @@ export const getLetterDetail = async (letter_id) => {
     }
 
     return letterDetail;
+};
+
+export const fetchHomeLetters = async (offset, limit) => {
+    return await getHomeLetters(offset, limit);
 };
