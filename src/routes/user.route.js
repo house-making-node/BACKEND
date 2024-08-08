c041c70a4ab54f9dbe23459f1691ec20c38974af
lilloo04 <lilloo04@naver.com>
2024-08-08 22:15:27 +0900

    refactor: user Router 통합

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,8 @@
 // user.route.js
 
 import express from "express";
-import asyncHandler from 'express-async-handler';
+import asyncHandler from "express-async-handler";
+import { userProfile } from "../controllers/user.controller.js";
 import { getScrapDetailsByUserId } from "../controllers/home_letters.controller.js";
 import { get } from "http";
 import { consultingStatusGet } from "../controllers/consult.controller.js";
@@ -9,6 +10,16 @@ import { getSharedLetterScrapList } from "../controllers/share_letters.controlle
 
 export const userRouter = express.Router();
 
-userRouter.get('/home_letters/scrap/:user_id', asyncHandler(getScrapDetailsByUserId));
-userRouter.get('/consulting/status/:consulting_id', asyncHandler(consultingStatusGet));
-userRouter.get('/:user_id/share_letters/scraps',asyncHandler(getSharedLetterScrapList)); // 마이페이지 공유레터 스크랩 리스트 조회
+userRouter.get("/profile/:user_id", userProfile); //마이페이지 유저 정보 조회
+userRouter.get(
+  "/home_letters/scrap/:user_id",
+  asyncHandler(getScrapDetailsByUserId)
+);
+userRouter.get(
+  "/consulting/status/:consulting_id",
+  asyncHandler(consultingStatusGet)
+);
+userRouter.get(
+  "/:user_id/share_letters/scraps",
+  asyncHandler(getSharedLetterScrapList)
+); // 마이페이지 공유레터 스크랩 리스트 조회
--- src/routes/user.router.js
+++
@@ -1,8 +0,0 @@
-// user.route.js
-
-import express from "express";
-import { userProfile } from "../controllers/user.controller.js";
-
-export const userRouter = express.Router();
-
-userRouter.get("/profile/:user_id", userProfile);
